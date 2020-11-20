ea887a483215a6de0b1dfd630bda1dfbad5e0fc8
Etby Gao <etby.org@gmail.com>
2020-11-20 10:39:09 +0800

    finish part2 courseinfo

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,44 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      <b>
+        Total of {parts.map((part) => part.exercises).reduce((a, b) => a + b)}{" "}
+        exercises
+      </b>
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
